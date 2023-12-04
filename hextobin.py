f205f72cc06cc04ce26073e2a90debb9ff41efd3
Harsh Soni <harshsoni13112003@gmail.com>
2023-12-04 22:07:16 +0530

    some changes

--- hextobin.py
+++ hextobin.py
@@ -10,6 +10,6 @@ def fun(a):
 		b=bin(s)[2:]
 		while (len(b) < 32):
 			b='0'+b
-		#print('Instruction:', i, '->', b[:5], b[5], b[6:10], b[10:14], b[14:16], b[16:])
+		print('Instruction:', i, '->', b[:5], b[5], b[6:10], b[10:14], b[14:16], b[16:])
 		print(opcode[int('0b'+b[:5],2)]+movu[int('0b'+b[14:16],2)], int('0b'+b[5],2), 'r'+str(int('0b'+b[6:10],2)), 'r'+str(int('0b'+b[10:14],2)), int('0b'+b[16:],2))
-		#print()
+		print()
